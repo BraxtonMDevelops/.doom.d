6bbfe6b744de23315e6936ea4787dc8975c17740
BraxtonMDevelops <braxton88888@gmail.com>
2021-11-11 18:13:02 -0500

    Fixed info popup issue. Stole modeline improvement

--- config.el
+++ config.el
@@ -27,7 +27,8 @@
 (use-package! info-colors
   :hook (Info-selection . info-colors-fontify-node))
 
-(set-popup-rules! '(("\\*info\\*" :ignore t)))
+(set-popup-rules! '(("^\\*info\\*$" ; Info buffers
+                     :ignore t)))
 
 (use-package! keycast
   :commands keycast-mode
@@ -47,6 +48,16 @@
     '(keycast-key :inherit custom-modified
                   :height 1.1
                   :weight)))
+
+(defun doom-modeline-conditional-buffer-encoding ()
+  "We expect the encoding to be LF UTF-8, so only show the modeline when this is not the case"
+  (setq-local doom-modeline-buffer-encoding
+              (unless (and (memq (plist-get (coding-system-plist buffer-file-coding-system) :category)
+                                 '(coding-category-undecided coding-category-utf-8))
+                           (not (memq (coding-system-eol-type buffer-file-coding-system) '(1 2))))
+                t)))
+
+(add-hook 'after-change-major-mode-hook #'doom-modeline-conditional-buffer-encoding)
 
 (setq org-directory "~/org/")
 
