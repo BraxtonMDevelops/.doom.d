86689a516fbd9ec5134675793fc1968bcb867136
BraxtonMDevelops <braxton8888@gmail.com>
2022-01-12 15:45:40 -0500

    miscellaneous changes in config

--- packages.el
+++ packages.el
@@ -1,3 +1,7 @@
+(package! emacsql-sqlite :built-in 'prefer)
+
 (package! info-colors :pin "47ee73cc19b1049eef32c9f3e264ea7ef2aaf8a5")
 
 (package! keycast :pin "04ba7519f34421c235bac458f0192c130f732f12")
+
+(package! nov :pin "b3c7cc28e95fe25ce7b443e5f49e2e45360944a3")
